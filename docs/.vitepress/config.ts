37a1c26bde1aa7e92dc267921de8013285d678a1
wbccb <wbccb18814122@163.com>
2023-04-19 22:58:41 +0800

    fix: 由于github的名称为CMS-docs，因此部署到github.io/CMS-docs，导致样式链接找不到

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "CMS-docs",
   description: "A VitePress Site",
-  base: "/cms-docs/",
+  base: "/CMS-docs/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
